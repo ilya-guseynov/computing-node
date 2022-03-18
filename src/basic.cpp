73b6f093f1c300ccd43a2bdf177b54fee507f17b
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-18 19:50:41 +0300

    new: add basic functions realizations and removed some

--- src/basic.cpp
+++ src/basic.cpp
@@ -1,6 +1,13 @@
 #include <vector>
+#include <algorithm>
 
 
+/**
+ * Calculates sum of numbers in provided vector.
+ *
+ * @param provided_vector Vector, which contains numbers.
+ * @return Sum of all values in provided vector.
+ */
 double calculate_vector_sum(std::vector<double> provided_vector) {
   double sum = 0;
 
@@ -9,6 +16,134 @@ double calculate_vector_sum(std::vector<double> provided_vector) {
   }
 
   return sum;
+}
+
+
+/**
+ * Subtracts elements from one another in array.
+ *
+ * e.g [5,3,1,-1] -> 5 - 3 - 1 - (-1) = 2
+ *
+ * @param provided_vector Collection of numbers.
+ * @return Difference.
+ */
+double calculate_vector_subtraction(std::vector<double> provided_vector) {
+  if (provided_vector.size() == 0) {
+    return 0;
+  }
+
+  if (provided_vector.size() == 1) {
+    return provided_vector[0];
+  }
+
+  double total = provided_vector[0];
+
+  for (unsigned int i = 1; i < provided_vector.size(); i++) {
+    total -= provided_vector[i];
+  }
+
+  return total;
+}
+
+
+/**
+ * Product of all elements in an array.
+ *
+ * @param provided_vector Collection of numbers.
+ * @return Product.
+ */
+double calculate_vector_product(std::vector<double> provided_vector) {
+  if (provided_vector.size() == 0) {
+    return 0;
+  }
+
+  if (provided_vector.size() == 1) {
+    return provided_vector[0];
+  }
+
+  double total = provided_vector[0];
+
+  for (unsigned int i = 1; i < provided_vector.size(); i++) {
+    if (provided_vector[i] == 0) {
+      return 0;
+    }
+
+    total *= provided_vector[i];
+  }
+
+  return total;
+}
+
+
+/**
+ * Factorial for some integer.
+ *
+ * @param number Integer.
+ * @return Result.
+ */
+double calculate_factorial(double number) {
+  double i = 2;
+  double o = 1;
+
+  while (i <= number) {
+    i *= i++;
+  }
+
+  return o;
+}
+
+
+/**
+ * Find maximum value in a vector.
+ *
+ * @param provided_vector Vector to be traversed.
+ * @return {Number} maximum value in the array.
+ */
+double get_max(std::vector<double> provided_vector) {
+  if (provided_vector.size() == 0) {
+    return 0;
+  }
+
+  if (provided_vector.size() == 1) {
+    return provided_vector[0];
+  }
+
+  double max = provided_vector[0];
+
+  for (unsigned int i = 1; i < provided_vector.size(); i++) {
+    if (provided_vector[i] > max) {
+      max = provided_vector[i];
+    }
+  }
+
+  return max;
+}
+
+
+/**
+ * Find minimum value in vector.
+ *
+ * @param provided_vector Vector to be traversed.
+ * @return {Number} minimum value in the array.
+ */
+double get_min(std::vector<double> provided_vector) {
+  if (provided_vector.size() == 0) {
+    return 0;
+  }
+
+  if (provided_vector.size() == 1) {
+    return provided_vector[0];
+  }
+
+  double min = provided_vector[0];
+
+  for (unsigned int i = 1; i < provided_vector.size(); i++) {
+    if (provided_vector[i] < min) {
+      min = provided_vector[i];
+    }
+  }
+
+  return min;
 }
 
 
--- src/basic.hpp
+++ src/basic.hpp
@@ -35,64 +35,12 @@ double calculate_vector_product(std::vector<double> provided_vector);
 
 
 /**
- * Calculate the binomial coefficient (n choose k)
- *
- * @param choices Available choices
- * @param chosen Number chosen
- * @return Number of possible choices
- */
-double calculate_binomail(double choices, double chosen);
-
-
-/**
  * Factorial for some integer.
  *
  * @param number Integer.
  * @return Result.
  */
 double calculate_factorial(double number);
-
-
-/**
- * Calculate the greastest common divisor amongst two integers.
- *
- * @param {Number} number A.
- * @param {Number} number B.
- * @return {Number} greatest common divisor for integers A, B.
- */
-double calculate_gcd(double a, double b);
-
-
-/**
- * Calculate the least common multiple amongst two integers.
- *
- * @param a A.
- * @param b B.
- * @return Least common multiple for integers A, B.
- */
-double calculate_lcm(double a, double b);
-
-
-/**
- * Retrieve a specified quantity of elements from an array, at random.
- *
- * @param numbers Set of values to select from.
- * @param quntity Quantity of elements to retrieve.
- * @param allow allow the same number to be returned twice.
- * @return Random elements.
- */
-std::vector<double> get_random(
-  std::vector<double> numbers, double quantity, bool allow
-);
-
-
-/**
- * Shuffle an vector.
- *
- * @param provided_vector Vector to be shuffled.
- * @return Shuffled vector.
- */
-std::vector<double> shuffle(std::vector<double> provided_vector);
 
 
 /**
@@ -111,93 +59,6 @@ double get_max(std::vector<double> provided_vector);
  * @return {Number} minimum value in the array.
  */
 double get_min(std::vector<double> provided_vector);
-
-
-/**
- * Create a range of numbers.
- *
- * @param start The start of the range.
- * @param stop The end of the range.
- * @param step The step of the range.
- * @return {Array} An array containing numbers within the range.
- */
-std::vector<double> range(double start, double stop, double step);
-
-
-/**
- * Calculate the divisor and modulus of two integers.
- *
- * @param a A.
- * @param b B.
- * @return [div, mod].
- */
-std::vector<double> div_mod(double a, double b);
-
-
-/**
- * Calculate:
- * if b >= 1: a^b mod m.
- * if b = -1: modInverse(a, m).
- * if b < 1: finds a modular rth root of a such that b = 1/r.
- *
- * @param a Number a.
- * @param b Number b.
- * @param m Modulo m.
- * @return See the above documentation for return values.
- */
-double power_mod(double a, double b, double m);
-
-
-/**
- * Calculate the extended Euclid Algorithm or extended GCD.
- *
- * @param a A.
- * @param b B.
- * @return [a, x, y] a is the GCD. x and y are the values such that
- * ax + by = gcd(a, b) .
- */
-std::vector<double> egcd(double a, double b);
-
-
-/**
- * Calculate the modular inverse of a number.
- *
- * @param a Number a.
- * @param m Modulo m.
- * @return If true, return number, else throw error.
- */
-double mod_inverse(double a, double m);
-
-
-/**
- * Determine is two numbers are equal within a given margin of precision.
- *
- * @param a First number.
- * @param b Second number.
- * @param e Epsilon.
- * @return true if equal, false if not.
- */
-bool numbers_equal(double a, double b, double e);
-
-
-/**
- * Calculate the falling factorial of a number.
- *
- * @param n Base
- * @param k Steps to fall
- * @return Result
- */
-double falling_factorial(double n, double k);
-
-
-/**
- * Calculate the permutation (n choose k)
- *
- * @param n Available choices
- * @param k Number chosen
- * @return Number of ordered variations
- */
-double permutation(double n, double k);
 
 
 /**
