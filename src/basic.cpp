52a4a6577d45130a1eb6ec6dfc464bfcd17a13b6
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-18 19:54:55 +0300

    new: add docstring to is_matrix_square realization

--- src/basic.cpp
+++ src/basic.cpp
@@ -147,6 +147,12 @@ double get_min(std::vector<double> provided_vector) {
 }
 
 
+/**
+ * Calculates if provided matrix is square or not.
+ *
+ * @param provided_matrix Vector of vectors, which represents a matrix.
+ * @return true if matrix is square, false if not.
+ */
 bool is_matrix_square(std::vector<std::vector<double>> matrix) {
   for (unsigned int i = 0; i < matrix.size(); i++) {
     if (matrix[i].size() != matrix.size()) {
