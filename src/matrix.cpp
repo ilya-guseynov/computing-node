1d9cb27f68bcb7117886b89884e14ec382021339
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-19 12:33:47 +0300

    new: implemented pure matrix_sum function

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -16,3 +16,31 @@ bool is_matrix_square(std::vector<std::vector<double>> matrix) {
 
   return true;
 }
+
+
+/**
+ * Add two matrices together. Matrices must be of same dimension.
+ *
+ * @param matrix_a Matrix A.
+ * @param matrix_b Matrix B.
+ * @return Summed matrix.
+ */
+std::vector<std::vector<double>> matrix_sum(
+  std::vector<std::vector<double>> matrix_a,
+  std::vector<std::vector<double>> matrix_b
+) {
+  std::vector<std::vector<double>> result_matrix;
+
+  for (unsigned int i = 0; i < matrix_a.size(); i++) {
+    std::vector<double> matrix_subarray;
+
+    for (unsigned int j = 0; i < matrix_a[i].size(); i++) {
+      matrix_subarray[j] = matrix_a[i][j] + matrix_b[i][j];
+    }
+
+    result_matrix.push_back(matrix_subarray);
+  }
+
+  return result_matrix;
+}
+
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -14,5 +14,18 @@
 bool is_matrix_square(std::vector<std::vector<double>> matrix);
 
 
+/**
+ * Add two matrices together. Matrices must be of same dimension.
+ *
+ * @param matrix_a Matrix A.
+ * @param matrix_b Matrix B.
+ * @return Summed matrix.
+ */
+std::vector<std::vector<double>> matrix_sum(
+  std::vector<std::vector<double>> matrix_a,
+  std::vector<std::vector<double>> matrix_b
+);
+
+
 #endif
 
